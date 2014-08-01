a345f449c867d6aabc32aded0cd5344319081d88
Nate Beaty <nate@clixel.com>
2014-08-01 16:10:13 -0500

    cruft

--- settings.py
+++ settings.py
@@ -21,12 +21,9 @@ DATABASE_HOST = ''             # Set to empty string for localhost. Not used wit
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. All choices can be found here:
-# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
 TIME_ZONE = 'America/Portland'
 
 # Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
@@ -36,19 +33,13 @@ SITE_URL = 'pdxguide.org'
 SERVER_EMAIL = 'www@%s' % SITE_URL
 
 # Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-# MEDIA_ROOT = '/var/www/pdxguide/media/'
 MEDIA_ROOT = '/var/www/pdxguide.org/public_html/media/'
-# MEDIA_ROOT = '/home/natebeaty/django/django_projects/pdxguide/media/'
-# MEDIA_ROOT = '/Users/natebeaty/django/django_projects/pdxguide/media/'
 
 # URL that handles the media served from MEDIA_ROOT.
-# Example: "http://media.lawrence.com"
 MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 # Make this unique, and don't share it with anybody.
@@ -58,7 +49,6 @@ SECRET_KEY = ''
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,12 +64,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'pdxguide.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates".
-    # Always use forward slashes, even on Windows.
-    # '/Users/natebeaty/django/django_templates',
-    # '/Users/natebeaty/django/django_projects/pdxguide/templates',
-    # '/home/natebeaty/django/django_templates',
-    # '/home/natebeaty/django/django_projects/pdxguide/templates',
     '/home/natebeaty/django/django_projects/pdxguide/templates',
 
 )
